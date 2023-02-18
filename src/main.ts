6075ec53edde7f8f0d5fe2387a1ec5f1cb756285
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-02-18 09:48:04 +0530

    fix hosting error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://locahost:3000'],
+    origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE','HEAD','OPTIONS'],
     credentials: true,
     exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
