8041d50fc1048294cf9bac563acd29ae9c788392
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-02-20 16:04:23 +0530

    changed the cors origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://127.0.0.1:3000'],
+    origin: ['http://54.236.8.228:5173'],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     credentials: true,
     exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
