fd850822a3ce65a309fef4dd6d8f574596158f67
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-02-20 12:03:01 +0530

    made some changes on cors policy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://kaleidoscopic-creponne-8feec3.netlify.app'],
+    origin: ['https://gleeful-hamster-4886dc.netlify.app'],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     credentials: true,
     exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
