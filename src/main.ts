355e666ff008f9b8a906c626df2c81c4e9f6ddff
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2023-02-22 20:38:55 +0530

    origin-updated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://main.d1irnkz1l4wgu5.amplifyapp.com'],
+    origin: ['https://hire.labonnz.club'],
     // origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     credentials: true,
